68fa8b4140f6ad6cc0ffbd6dc7e2aee76c910b5e
William Gaul <willyg302@gmail.com>
2013-12-02 21:43:01 -1000

    Uploading the Noat I'm using now

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 Noat
 ====
 
-A dead-simple app for CRUD-ing tiny bits of information
+A dead-simple app for CRUD-ing tiny bits of information. Some features:
+
+* Powered by App Engine, themed by Bootstrap
+* Uses [Summernote](http://hackerwins.github.io/summernote/) for sweet WYSIWYG editing
+* Very simple interface (add a note, edit it, or delete it...that's it!)
+* Mobile-friendly
+
+Setup
+-----
+
+Open up **app.yaml** and change `[YOUR APPLICATION HERE]` to the name of your own App Engine application.
+
+After deploying to App Engine, you will need to set up an "author" that will be the ancestor of all notes written in Noat. To do this, navigate to `yourapp.appspot.com/admin/`, login using your Google credentials, and visit the Interactive Console. Run the following code:
+
+```
+from google.appengine.ext import ndb
+from noat import Author
+
+newauthor = Author(author_name='[AUTHOR NAME HERE]', author_pass='[HASHED PASS HERE]')
+newauthor.put()
+```
+
+A good way to get a password is by hashing it using SHA-256:
+
+```
+import hashlib
+
+print hashlib.sha256([AUTHOR PASS HERE] + [SECRET SALT]).hexdigest()
+```
+
+You can then use the output of the above code for the value of `[HASHED PASS HERE]`.
+
+Using Noat
+----------
+
+Visit yourapp.appspot.com/rnote?p=[HASHED PASS HERE]&n=[AUTHOR NAME HERE].
+
+There is no login form; Noat just uses the username and password provided via the URL to retrieve notes. Since this isn't secure from eavesdropping, Noat is not recommended for storing sensitive information.
+
+That being said, this makes Noat very easy to use. Just bookmark the above URL in all your browsers and you've got all your notes synced everywhere. Noat also includes Apple icon support, so you can create an application icon on iOS devices for one-tap access.
+
+Why Noat?
+---------
+
+I needed a way of having small bits of information available to me at all times, from any device I owned, without all the fuss of a big desktop application, login forms, and the like. I needed something in between Pastebin (with a bit more security and better markup features) and Wunderlist (without all the complexity).
+
+Before making Noat, I used to rely on email to fill the void. Sometimes I would be at school and find a neat link that I'd want to revisit later at home. How could I communicate the link to myself? By syncing bookmarks? By adding it to a file in my Dropbox? No...by emailing myself the link. It seems old-fashioned, but it was really the easiest way to go.
+
+Now, I just click on my Noat bookmark, add a new note, paste the link, and save. Even easier!
+
+Why "Noat"?
+-----------
+
+There seems to be a recent software development trend of abusing the English language in every way possible when coming up with app names. Noat continues this tradition in style. Plus note rhymes with goat. I mean, isn't that kind of obvious?
---
+++ noat.py
@@ -0,0 +1,113 @@
+import os, urllib, jinja2, webapp2, re
+from google.appengine.ext import ndb
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'])
+
+DEFAULT_AUTHOR_NAME = 'anonymous'
+DEFAULT_AUTHOR_PASS = 'password'
+
+
+class Author(ndb.Model):
+	author_name = ndb.StringProperty()
+	author_pass = ndb.StringProperty()
+
+
+class Note(ndb.Model):
+	"""Models a single note that can be added to Parrot Notes"""
+	note_title = ndb.StringProperty(indexed=False)
+	note_content = ndb.TextProperty()
+	note_type = ndb.StringProperty()
+	date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+def get_author_info(request):
+	author = None
+	author_name = request.get('n', DEFAULT_AUTHOR_NAME)
+	author_pass = request.get('p', DEFAULT_AUTHOR_PASS)
+	if author_name != DEFAULT_AUTHOR_NAME and author_pass != DEFAULT_AUTHOR_PASS:
+		author = Author.query().filter(Author.author_name == author_name).filter(Author.author_pass == author_pass).get()
+	return author, author_name, author_pass
+
+def nl2br(value):
+	return value.replace('\n', '<br>\n')
+
+JINJA_ENVIRONMENT.filters['nl2br'] = nl2br
+
+
+class MainPage(webapp2.RequestHandler):
+
+	def get(self):
+		self.response.write('')
+
+
+class ReadNote(webapp2.RequestHandler):
+
+	def get(self):
+		notes = []
+		author, author_name, author_pass = get_author_info(self.request)
+
+		if author != None:
+			notes = Note.query(ancestor=author.key).order(-Note.date).fetch(50)
+
+		template_values = {
+			'notes': notes,
+			'author_name': author_name,
+			'author_pass': author_pass,
+		}
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+
+
+# Either Creates or Updates a note, based on whether a unique note ID is given
+class EditNote(webapp2.RequestHandler):
+
+	def post(self):
+		author, author_name, author_pass = get_author_info(self.request)
+
+		if author != None:
+			key_id = self.request.get('k', '')
+			note_title = self.request.get('title')
+			note_content = self.request.get('content')
+			note_type = self.request.get('type').lower()
+			if key_id:
+				oldnote = Note.get_by_id(int(key_id), parent=author.key)
+				oldnote.populate(note_title=note_title, note_content=note_content, note_type=note_type)
+				oldnote.put()
+			else:
+				newnote = Note(parent=author.key, note_title=note_title, note_content=note_content, note_type=note_type)
+				newnote.put()
+
+		query_params = {
+			'n': author_name,
+			'p': author_pass,
+		}
+		self.redirect('/rnote?' + urllib.urlencode(query_params))
+
+
+class DeleteNote(webapp2.RequestHandler):
+
+	def post(self):
+		author, author_name, author_pass = get_author_info(self.request)
+
+		if author != None:
+			key_id = self.request.get('k', '')
+			if key_id:
+				Note.get_by_id(int(key_id), parent=author.key).key.delete()
+
+		query_params = {
+			'n': author_name,
+			'p': author_pass,
+		}
+		self.redirect('/rnote?' + urllib.urlencode(query_params))
+
+
+application = webapp2.WSGIApplication([
+	('/', MainPage),
+	('/index.html', MainPage),
+	('/rnote', ReadNote),
+	('/enote', EditNote),
+	('/dnote', DeleteNote),
+], debug=True)
